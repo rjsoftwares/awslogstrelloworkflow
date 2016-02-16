135527277b1369f3cc719145981afd1cba744683
Rajat Agarwalla <ra@rjs.in>
2016-02-16 15:14:47 +0530

    Initial commit of scripts.

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# awslogstrelloworkflow
-Create a Trello workflow using your Amazon Web Services (AWS) Elastic Load Balancer (ELB) Logs. You may modify this to process standard Apache Logs as well.
+# About this script
+
+This is a simple script for improving error handling of your web service hosted on Amazon Web Services using Elastic Load Balancers. Here's how it works:
+
+* Download the AWS ELB logs from the S3 bucket.
+* Parse the log files and find out exceptions based on user defined parameters.
+* Create Trello cards for the exceptions [execution time, 404 & 500 errors].
+* In case card already present, add the exception as a comment.
+* In case card was marked as "Completed", transfer it to a "Redo" list.
+* Add the card to the developer who is going to fix it.
+
+This script can be easily modified for any Apache log based on your requirements. 
+
+
+# Enable Access Logs
+
+To get AWS reports, first of all you will need to enable access logs for your load balancer. Details on how to enable these are present on the following link:
+
+http://docs.aws.amazon.com/ElasticLoadBalancing/latest/DeveloperGuide/enable-access-logs.html
+
+# Create AWS Access Key
+
+To create an access key for your AWS account : 
+
+* Use your AWS account email address and password to sign in to the AWS Management Console.
+* In the upper-right corner of the console, click the arrow next to the account name or number and then click Security Credentials.
+* On the AWS Security Credentials page, expand the Access Keys (Access Key ID and Secret Access Key) section.
+* Click Create New Access Key. 
+	** Note: you can have a maximum of two access keys (active or inactive) at a time.
+* Click Download Key File to save the access key ID and secret key to a .csv file on your computer.
+* Attach a security policy to the new access key that grants permissions for the appropriate S3 bucket that contains AWS logs.
+
+# Get Trello Credentials
+
+To get Api key follow the below steps:
+
+* Log into your trello account.
+* Visit https://trello.com/app-key
+* Here you will find "Developer API Key" of Trello.
+
+To get a write access token:
+* Go to https://trello.com/1/connect?key=[DEVELOPER_APP_KEY]&name=MyApp&response_type=token&scope=read,write
+* It will ask for permissions. Once you allow those, You will get the access token in response.
+
+To get a list id:
+* Open your trello board.
+* append ".json" to the url and press enter.
+* You will get all details of that board in a json format.
+* Search for "lists" in the json. In this lists array you will get the details of your lists (like id, name etc).
+
+# Configure Script
+
+The script configuration is simple. Just setup the AWS and Trello credentials in the script.
+
+
+# USAGE
+
+Once the script is configured it should be run via cron every hour. To execute errorlog.php every 1 hour use the following command:
+
+* crontab -e 00 * * * * /path/to/php /path/to/RJSErrorLog.php
---
+++ errorlog.php
@@ -0,0 +1,234 @@
+<?php
+set_time_limit(0);
+ini_set('memory_limit', '512M');
+
+/* === Created at RJS, Kolkata   === */
+/* === R.Chaudhury, T.Chatterjee === */
+
+/*=============== Config Amazon Web Services Credentials ===================*/
+
+define("aws_bucket_name", "YOUR_AWS_BUCKET"); // Your AWS Bucket name
+define('awsAccessKey', 'YOUR_AWS_ACCESS_KEY_ID'); // Your AWS Access key
+define('awsSecretKey', 'YOUR_AWS_SECRET_ACCESS_KEY'); // Your AWS Secret key
+$aws_folder_path = "AWSLogs/[AWS-ACCOUNT-ID]/elasticloadbalancing/[REGION]/"; // Aws logfiles path
+$sdk_path        = "/your/aws/sdk/path/filename.php"; //Aws SDK Path
+
+
+/*========================================================================*/
+
+/*=============== Config Trello Credentials ===================*/
+
+$member_array = array(
+    'YOUR_MEMBER_ID_1' => '@member_1_to_tag',
+    'YOUR_MEMBER_ID_2' => '@member_2_to_tag'
+);
+
+// Need to Configure Trello memberids and members to tag 
+
+$trello_api_key                     = ''; // Trello Developer Api Key.
+$trello_api_endpoint                = 'https://api.trello.com/1'; // Trello Api endpoint.
+$trello_list_id_for_newScriptErrors = 'TRELLO_LIST_ID_FOR_NEW_SCRIPT_ERRORS'; // List id of New Script errors
+$trello_list_id_for_errorsResolved  = 'TRELLO_LIST_ID_FOR_RESOLVED_ERRORS'; // List id of Completed tasks
+$trello_list_id_for_redoTasks       = 'TRELLO_LIST_ID_FOR_REDO_TASKS'; // List id of redo tasks 
+$trello_list_id_for_ignore          = 'TRELLO_LIST_ID_FOR_IGNORE_ERRORS'; // List id of ignore tasks
+$trello_access_token                = 'YOUR_TRELLO_ACCESS_TOKEN'; //Trello member access token for authentication
+$completed_task_level_id            = "TRELLO_COMPLETED_TASK_LEBEL_ID"; //Lebel id for completed tasks
+
+/*============================================================*/
+
+/*=============== Need to config as required ===================*/
+
+$errorcode_array = array(
+    '404' => '56b42d06152c3f92fdd73ac3',
+    '500' => '56b42d06152c3f92fdd73ac4',
+    'slow_process' => '56b42d06152c3f92fdd73ac0'
+);
+
+//Errors to found with trello label id
+
+$maxprocessing_time = 3; // Maximum Backend Processing Time You want to fetch from error log in seconds
+
+/*============================================================*/
+
+include_once($sdk_path);
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+$s3 = S3Client::factory(array(
+    'key' => awsAccessKey,
+    'secret' => awsSecretKey
+));
+
+$date          = date("Y/m/d/", strtotime("-1 hour"));
+$prefix_folder = $aws_folder_path . $date;
+
+$objects = $s3->getListObjectsIterator(array(
+    'Bucket' => aws_bucket_name,
+    'Prefix' => $prefix_folder
+));
+foreach ($objects as $object) {
+    $log = $object['Key'];
+    $ext = pathinfo($log, PATHINFO_EXTENSION);
+    if ($ext == 'log') {
+        $result     = $s3->getObject(array(
+            'Bucket' => aws_bucket_name,
+            'Key' => $log
+        ));
+        $logbody    = $result['Body'];
+        $logbodyarr = array();
+        $logbodyarr = explode("\n", $logbody);
+        foreach ($logbodyarr as $line) {
+            $parts           = explode(' ', $line);
+            $errors_1        = trim($parts['7']);
+            $errors_2        = trim($parts['8']);
+            $background_time = trim($parts['5']);
+            $site_name       = trim(urldecode($parts['12']));
+            $site_name_arr   = explode("?", $site_name);
+            $site_name       = $site_name_arr['0'];
+            $query_string    = $site_name_arr['1'];
+            if (array_key_exists($errors_1, $errorcode_array) && $errors_1 == $errors_2) {
+                if (empty($error_log[$site_name])) {
+                    $error_log[$site_name][$query_string] = array_fill_keys(array_keys($errorcode_array), 0);
+                    $error_log[$site_name][$query_string][$errors_1]++;
+                } else {
+                    $error_log[$site_name][$query_string][$errors_1]++;
+                }
+            }
+            
+            if ($background_time > $maxprocessing_time && $maxprocessing_time > 0) {
+                $error_log[$site_name][$query_string]['slow_process'] = $background_time;
+            }
+        }
+        $result = $s3->deleteObject(array(
+            'Bucket' => aws_bucket_name,
+            'Key' => $log
+        ));
+        
+    }
+}
+
+if (!empty($error_log)) {
+    $member_to_tag        = implode(" ", $member_array);
+    $membersid_to_add     = implode(",", array_keys($member_array));
+    $comments_arr         = array();
+    $card                 = getTrelloCards("$trello_api_endpoint/lists/$trello_list_id_for_newScriptErrors/cards?key=$trello_api_key&token=$trello_access_token");
+    $completed_list_cards = getTrelloCards("$trello_api_endpoint/lists/$trello_list_id_for_errorsResolved/cards?key=$trello_api_key&token=$trello_access_token");
+    $redo_list_cards      = getTrelloCards("$trello_api_endpoint/lists/$trello_list_id_for_redoTasks/cards?key=$trello_api_key&token=$trello_access_token");
+    $ignore_list_cards    = getTrelloCards("$trello_api_endpoint/lists/$trello_list_id_for_ignore/cards?key=$trello_api_key&token=$trello_access_token");
+    foreach ($error_log as $key => $value) {
+        $trello_msg    = '';
+        $labels        = '';
+        $site_error    = array();
+        $check_list    = 0;
+        foreach ($value as $query_string => $errors) {
+            $trello_msg .= $query_string . " ";
+            foreach ($errors as $err_code => $total_err) {
+                if ($total_err != 0) {
+                    if ($err_code == 'slow_process')
+                        $trello_msg .= "(Threshold time - " . $total_err . ") \n";
+                    else
+                        $trello_msg .= "($err_code Error - " . $total_err . ") \n";
+                    $labels .= "," . $errorcode_array[$err_code];
+                }
+                $site_error[$err_code] = $total_err;
+            }
+            $trello_msg .= "\n \n";
+            $check_list++;
+            if ($check_list == 50)
+                break;
+        }
+        $labels = trim($labels, ",");
+        if (in_array($key, $card) || in_array($key, $completed_list_cards) || in_array($key, $redo_list_cards) || in_array($key, $ignore_list_cards)) {
+            $card_id = array_search($key, $card);
+            if (in_array($key, $redo_list_cards)) {
+                $card_id = array_search($key, $redo_list_cards);
+            }
+            if (in_array($key, $completed_list_cards)) {
+                $card_id          = array_search($key, $completed_list_cards);
+                $postfields_array = array(
+                    'key' => $trello_api_key,
+                    'token' => $trello_access_token,
+                    'value' => $trello_list_id_for_redoTasks
+                );
+                postToTrello("$trello_api_endpoint/cards/$card_id/idList", $postfields_array, "PUT");
+                foreach ($site_error as $site_err => $total_error) {
+                    if ($total_error != '0') {
+                        $postfields_array = array(
+                            'key' => $trello_api_key,
+                            'token' => $trello_access_token,
+                            'value' => $errorcode_array[$site_err]
+                        );
+                        postToTrello("$trello_api_endpoint/cards/$card_id/idLabels", $postfields_array, "POST");
+                    }
+                }
+            }
+            if (in_array($key, $ignore_list_cards)) {
+                $card_id          = array_search($key, $ignore_list_cards);
+                $postfields_array = array(
+                    'key' => $trello_api_key,
+                    'token' => $trello_access_token,
+                    'idLabel' => $completed_task_level_id
+                );
+                postToTrello("$trello_api_endpoint/cards/$card_id/idLabels/$completed_task_level_id", $postfields_array, "DELETE");
+                
+                
+            }
+            if ($comments_arr[$card_id] < 50) {
+                $postfields_array = array(
+                    'key' => $trello_api_key,
+                    'token' => $trello_access_token,
+                    'text' => "$member_to_tag \n $trello_msg"
+                );
+                postToTrello("$trello_api_endpoint/cards/$card_id/actions/comments", $postfields_array, "POST");
+            }
+        } else {
+            $postfields_array = array(
+                'key' => $trello_api_key,
+                'token' => $trello_access_token,
+                'idList' => $trello_list_id_for_newScriptErrors,
+                'name' => $key,
+                'idMembers' => $membersid_to_add,
+                'idLabels' => $labels
+            );
+            $add_new_card     = postToTrello("$trello_api_endpoint/cards", $postfields_array, "POST");
+            $new_card_id      = $add_new_card['id'];
+            $postfields_array = array(
+                'key' => $trello_api_key,
+                'token' => $trello_access_token,
+                'text' => "$member_to_tag \n $trello_msg"
+            );
+            postToTrello("$trello_api_endpoint/cards/$new_card_id/actions/comments", $postfields_array, "POST");
+            
+        }
+        
+    }
+} else {
+    echo "No data found";
+}
+
+function postToTrello($url, $curlopt_postfields, $request){
+    $ch = curl_init($url);
+    curl_setopt_array($ch, array(
+        CURLOPT_CUSTOMREQUEST => $request,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => http_build_query($curlopt_postfields)
+    ));
+    $result = curl_exec($ch);
+    $result = json_decode($result, true);
+    return $result;
+}
+
+function getTrelloCards($url){
+    global $comments_arr;
+    $trello_cards  = array();
+    $all_cards     = file_get_contents($url);
+    $all_cards_arr = json_decode($all_cards, true);
+    foreach ($all_cards_arr as $key => $value) {
+        $trello_cards[$value['id']] = $value['name'];
+        $comments_arr[$value['id']] = $value['badges']['comments'];
+    }
+    return $trello_cards;
+}
+
+?>
